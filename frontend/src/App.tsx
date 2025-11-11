750ed629b4d25e3131a3891cb3351b876fdaef07
mariusfredrichsen <mariusfredrichsen@gmail.com>
2025-11-11 15:37:26 +0100

    asd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function find_last_word(sentence: String): String {
+  if (sentence.length === 0) {
+    return "";
+  } else {
+    const splitted = sentence.split(" ");
+    const last = splitted.at(-1)
+    if (last) {
+      return last;
+    } else {
+      return "";
+    }
+  }
+}
+
+function suggest_words(sentence: String): String[] {
+  const last_word = find_last_word(sentence);
+  // put in logic here
+  const suggested_words = [last_word, "2", "1", "3"];
+  return suggested_words;
+}
+
+function InputField() {
+  const [inputValue, setInputValue] = useState("")
+
+  return (
+    <div className="w-full h-full border-2 border-black">
+      <div className="flex w-full justify-center items-center space-x-2">
+        {suggest_words(inputValue).map((word, idx) => (
+          <>
+            <span key={idx} className="px-2 py-1">
+              {word}
+            </span>
+            <span>|</span>
+          </>
+        ))}
+      </div>
+      <input
+        className="w-full border border-gray-300"
+        value={inputValue}
+        onChange={e => setInputValue(e.currentTarget.value)}
+      />
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="min-h-screen flex items-center justify-center">
+      <header className="text-center">
+        <h1 className="text-2xl font-bold mb-4">Very cool autocorrect demo!</h1>
+        <div>
+          <InputField />
+        </div>
       </header>
     </div>
   );
